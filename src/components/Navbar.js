9314b99f81c95642851416a2b79efce4aa9f6501
hwalk1 <haydenjwalker1@gmail.com>
2022-07-26 15:52:01 +1000

    Added dayt logo & styling for the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "../Dayt-170x50px.svg"
+
+
+export default function Navbar() {
+  return (
+    <div className='navbar--main'>
+      <img src={logo} alt="Logo" />
+    </div>
+  )
+}
